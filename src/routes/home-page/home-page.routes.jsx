04222d44822aa9f78899abc824b650e666db5c0b
Mateusz-best-creator <114943990+Mateusz-best-creator@users.noreply.github.com>
2023-03-26 14:52:06 +0200

    creating home page site

--- src/routes/home-page/home-page.routes.jsx
+++ src/routes/home-page/home-page.routes.jsx
@@ -1,9 +1,13 @@
 import React, {useEffect, useRef} from "react";
 
-import VideoPlayer from "../../video-player/video-player.component";
+import { Link } from "react-router-dom";
 
-// import Typed from "typed.js";
+import Footer from '../../components/footer/footer.component';
 
+import Typed from "typed.js";
+
+// astronaut
+import astronaut from '../../assets/astronaut.png'
 
 import './home-page.styles.scss';
 
@@ -11,32 +15,42 @@ const HomePage = () => {
 
     const el = useRef(null);
 
-  // useEffect(() => {
-  //   const typed = new Typed(el.current, {
-  //     strings: ["Front End Developer", "Python Developer", "Hardworking Guy"],
-  //     startDelay: 300,
-  //     typeSpeed: 150,
-  //     backSpeed: 150,
-  //     backDelay: 150,
-  //     smartBackspace: true,
-  //     loop: true,
-  //     showCursor: false,
-  //   });
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["JavaScript Developer.", "React Developer."],
+      startDelay: 300,
+      typeSpeed: 150,
+      backSpeed: 150,
+      backDelay: 150,
+      smartBackspace: true,
+      loop: true,
+      showCursor: false,
+    });
 
-  //   // Destropying
-  //   return () => {
-  //     typed.destroy();
-  //   };
-  // }, []);
+    // Destropying
+    return () => {
+      typed.destroy();
+    };
+  }, []);
     
   return (
     <>
       <div className="home-page-container">
-        <div className="Content">
-          <h6>
-            Hello I'm Mateusz Wieczorek, <br /><span ref={el}></span>
+        <div className="home-page-name">
+          <h6 className='dynamic-text'>
+            Hello I'm Mateusz <a style={{color: 'black', fontSize: '2rem'}} href='https://github.com/Mateusz-best-creator?tab=repositories' target='_blank'><i className="fab fa-github"></i></a> Wieczorek, <br /><span style={{color: 'rgba(231, 111, 12, 1)'}} ref={el}></span>
           </h6>
-          <VideoPlayer />
+          <p className='home-page-text'>
+            I am a front end developer. I love building 
+            attractive and effective web applications.
+            Hit the button below to see what I can do for your business.
+          </p>
+          <Link to='projects-page'>
+            <button className='home-page-button'>Check My Work</button>
+          </Link>
+        </div>
+        <div className='home-page-photo'>
+          <img className='astronaut-image' style={{width: '400px'}} src={astronaut} alt='' />
         </div>
       </div>
     </>
